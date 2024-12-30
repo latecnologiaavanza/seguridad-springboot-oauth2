6ac6c7d67a1addaa40d389c6b8cac31ea51f0060
latecnologiaavanza <cramirezchristian77@gmail.com>
2024-12-30 01:28:31 -0500

    Seguridad OAuth2

---
+++ src/main/java/com/lta/securityservice/config/RsaKeysConfig.java
@@ -0,0 +1,11 @@
+package com.lta.securityservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+//Vinculamos propiedades de configuracion con campos de la clase RsaKeysConfig
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeysConfig(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+}
